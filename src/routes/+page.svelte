33aaaf8e239eabebb65b468a24e2bd7c1aa0dce7
James-Woodland <james.woodland@students.plymouth.ac.uk>
2023-04-06 18:43:28 +0100

    Added last played layout to main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,28 +14,118 @@
 			</div>
 			<div class = "flex flex-row h-40">
 				<a href="" class="bg-slate-950 w-1/5 h-30 mr-3 mt-3 mb-3 hover:bg-gray-100 rounded-lg">
-					<div class="">
-
+					<div class="flex h-full">
+						<div class="flex flex-col w-1/3">
+							<div class="w-full h-4/5">
+								<img src="./TeamIcons/diamant.webp" alt="" class="w-40 h-full object-none object-right">
+							</div>
+							<div class="w-full h-1/5 text-red-700 font-bold ml-2">
+								Scrim
+							</div>
+						</div>
+						<div class="w-2/3 h-full flex flex-col">
+							<div class = "h-2/5 w-full text-red-700 font-bold text-right text-5xl pr-2">
+								Loss
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								0:1
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								06/4/23
+							</div>										
+						</div>
 					</div>
 				</a>
 				<a href="" class="bg-slate-950 w-1/5 h-30 m-3 hover:bg-gray-100 rounded-lg">
-					<div class="">
-
+					<div class="flex h-full">
+						<div class="flex flex-col w-1/3">
+							<div class="w-full h-4/5">
+								<img src="./TeamIcons/diamant.webp" alt="" class="w-40 h-full object-none object-right">
+							</div>
+							<div class="w-full h-1/5 text-red-700 font-bold mx-2">
+								Scrim
+							</div>
+						</div>
+						<div class="w-2/3 h-full flex flex-col">
+							<div class = "h-2/5 w-full text-red-700 font-bold text-right text-5xl pr-2">
+								Loss
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								0:1
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								06/4/23
+							</div>											
+						</div>
 					</div>
 				</a>
 				<a href="" class="bg-slate-950 w-1/5 h-30 m-3 hover:bg-gray-100 rounded-lg">
-					<div class="">
-
+					<div class="flex h-full">
+						<div class="flex flex-col w-1/3">
+							<div class="w-full h-4/5">
+								<img src="./TeamIcons/diamant.webp" alt="" class="w-40 h-full object-none object-right">
+							</div>
+							<div class="w-full h-1/5 text-red-700 font-bold ml-2">
+								Scrim
+							</div>
+						</div>
+						<div class="w-2/3 h-full flex flex-col">
+							<div class = "h-2/5 w-full text-red-700 font-bold text-right text-5xl pr-2">
+								Loss
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								0:1
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								06/4/23
+							</div>											
+						</div>
 					</div>
 				</a>
 				<a href="" class="bg-slate-950 w-1/5 h-30 m-3 hover:bg-gray-100 rounded-lg">
-					<div class="">
-
+					<div class="flex h-full">
+						<div class="flex flex-col w-1/3">
+							<div class="w-full h-4/5">
+								<img src="./TeamIcons/diamant.webp" alt="" class="w-40 h-full object-none object-right">
+							</div>
+							<div class="w-full h-1/5 text-red-700 font-bold ml-2">
+								Official
+							</div>
+						</div>
+						<div class="w-2/3 h-full flex flex-col">
+							<div class = "h-2/5 w-full text-red-700 font-bold text-right text-5xl pr-2">
+								Loss
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								0:1
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								06/4/23
+							</div>											
+						</div>
 					</div>
 				</a>
 				<a href="" class="bg-slate-950 w-1/5 h-30 m-3 hover:bg-gray-100 rounded-lg">
-					<div class="">
-
+					<div class="flex h-full">
+						<div class="flex flex-col w-1/3">
+							<div class="w-full h-4/5">
+								<img src="./TeamIcons/diamant.webp" alt="" class="w-40 h-full object-none object-right">
+							</div>
+							<div class="w-full h-1/5 text-red-700 font-bold ml-2">
+								Scrim
+							</div>
+						</div>
+						<div class="w-2/3 h-full flex flex-col">
+							<div class = "h-2/5 w-full text-red-700 font-bold text-right text-5xl pr-2">
+								Loss
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								0:1
+							</div>
+							<div class = "h-1/5 w-full text-red-700 font-bold text-right pr-2">
+								06/4/23
+							</div>										
+						</div>
 					</div>
 				</a>
 			</div>
@@ -43,6 +133,10 @@
 		</div>
 		<div class="text-3xl font-bold text-red-700">
 			Stats
+			<div class = "flex flex-col">
+				
+
+			</div>
 		</div>
 		<div class="text-3xl font-bold text-red-700">
 			Solo Queue
@@ -50,9 +144,6 @@
 		
 	</div>
 </main>
-
-	
-
 
 <svelte:head>
 	<link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"/>
