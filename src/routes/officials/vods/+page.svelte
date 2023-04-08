3a7e7af60cc81c2dc0160d0c06d4577fa887982d
James-Woodland <james.woodland@students.plymouth.ac.uk>
2023-04-08 12:31:31 +0100

    added new pages for scrims and officials

---
+++ src/routes/officials/vods/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import { TestimonialPlaceholder } from 'flowbite-svelte';
+	import Layout from '../../Layout.svelte'
+	import "../../../app.css";
+</script>
+
+<main class="bg-slate-800 flex h-full flex-row overflow-auto">
+	<Layout/>
+</main>
